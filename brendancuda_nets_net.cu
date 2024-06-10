4914bf75d682d885dac35c1bd75bd5ea4bb5043d
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-06-10 19:40:28 -0400

    Added CUDA compatibility and destructors

--- brendancuda_nets_net.cu
+++ brendancuda_nets_net.cu
@@ -1,6 +1,7 @@
 #include "brendancuda_nets_net.cuh"
+#include <device_launch_parameters.h>
 
-BrendanCUDA::Nets::NetNode::NetNode() {
+__host__ __device__ BrendanCUDA::Nets::NetNode::NetNode() {
     data = 0;
     inputs = 0;
     inputCount = 0;
@@ -8,15 +9,35 @@ BrendanCUDA::Nets::NetNode::NetNode() {
     outputCount = 0;
 }
 
+__host__ __device__ void BrendanCUDA::Nets::NetNode::Destroy(dataDestructor_t DataDestructor) {
+    if (DataDestructor) {
+        DataDestructor(data);
+    }
+#if __CUDA_ARCH__
+    delete[] inputs;
+    delete[] outputs;
+#else
+    cudaFree(inputs);
+    cudaFree(outputs);
+#endif
+}
+
 BrendanCUDA::Nets::Net::Net() 
     : nodes(*(new thrust::device_vector<NetNode>())) {}
 
-void BrendanCUDA::Nets::Net::Destroy() {
+__global__ void destroyKernel(BrendanCUDA::Nets::NetNode* nodes, BrendanCUDA::Nets::dataDestructor_t DataDestructor) {
+    nodes[blockIdx.x].Destroy(DataDestructor);
+}
+
+void BrendanCUDA::Nets::Net::Destroy(dataDestructor_t DataDestructor) {
+    destroyKernel<<<nodes.size(), 1>>>(nodes.data().get(), DataDestructor);
     delete (&nodes);
 }
+
 thrust::device_ptr<BrendanCUDA::Nets::NetNode> BrendanCUDA::Nets::Net::Data() {
     return nodes.data();
 }
+
 thrust::device_reference<BrendanCUDA::Nets::NetNode> BrendanCUDA::Nets::Net::operator[](size_t i) {
     return nodes[i];
 }
--- brendancuda_nets_net.cuh
+++ brendancuda_nets_net.cuh
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <thrust/device_vector.h>
+#include <cuda_runtime.h>
 
 namespace BrendanCUDA {
     namespace Nets {
+        using dataDestructor_t = void (*)(void*);
         struct NetNode final {
             void* data;
             size_t* inputs;
@@ -11,12 +13,14 @@ namespace BrendanCUDA {
             size_t* outputs;
             size_t outputCount;
 
-            NetNode();
+            __host__ __device__ NetNode();
+
+            __host__ __device__ void Destroy(dataDestructor_t DataDestructor);
         };
         class Net final {
         public:
             Net();
-            void Destroy();
+            void Destroy(dataDestructor_t DataDestructor);
             thrust::device_ptr<NetNode> Data();
             thrust::device_reference<NetNode> operator[](size_t i);
         private:
