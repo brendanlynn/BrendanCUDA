76a86e3cb40f56c8baebad329eb14d93c3a938f8
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-06-14 16:32:39 -0400

    Replaced redundant code with function call

--- brendancuda_nets_net.cu
+++ brendancuda_nets_net.cu
@@ -393,56 +393,16 @@ void BrendanCUDA::Nets::Net::RemoveAt(size_t Index, dataDestructor_t DataDestruc
         throw std::exception();
     }
     for (size_t i = 0; i < nn.inputCount; ++i) {
-        NetNode nn2 = nodes[inputs[i]];
-        size_t* outputs2 = new size_t[nn2.outputCount];
-        size_t ocm1 = nn2.outputCount - 1;
-        if (cudaMemcpy(outputs2, nn2.outputs, sizeof(size_t) * nn2.outputCount, cudaMemcpyDeviceToHost)) {
-            throw std::exception();
+        size_t o = inputs[i];
+        if (o != Index) {
+            RemoveConnection_OnlyInput(o, Index, false);
         }
-        for (size_t j = 0; j < nn2.outputCount; ++j) {
-            if (outputs2[j] == Index) {
-                outputs2[j] = outputs[ocm1];
-                break;
-            }
-        }
-        size_t* outputs3;
-        if (cudaMalloc(&outputs3, sizeof(size_t) * ocm1)) {
-            throw std::exception();
-        }
-        if (cudaMemcpy(outputs3, outputs2, sizeof(size_t) * ocm1, cudaMemcpyDeviceToHost)) {
-            throw std::exception();
-        }
-        cudaFree(nn2.outputs);
-        nn2.outputs = outputs3;
-        nn2.outputCount = ocm1;
-        nodes[inputs[i]] = nn2;
-        delete[] outputs2;
     }
     for (size_t i = 0; i < nn.outputCount; ++i) {
-        NetNode nn2 = nodes[outputs[i]];
-        size_t* inputs2 = new size_t[nn2.inputCount];
-        size_t icm1 = nn2.inputCount - 1;
-        if (cudaMemcpy(inputs2, nn2.inputs, sizeof(size_t) * nn2.inputCount, cudaMemcpyDeviceToHost)) {
-            throw std::exception();
+        size_t o = outputs[i];
+        if (o != Index) {
+            RemoveConnection_OnlyOutput(Index, o, false);
         }
-        for (size_t j = 0; j < nn2.inputCount; ++j) {
-            if (inputs2[j] == Index) {
-                inputs2[j] = inputs[icm1];
-                break;
-            }
-        }
-        size_t* inputs3;
-        if (cudaMalloc(&inputs3, sizeof(size_t) * icm1)) {
-            throw std::exception();
-        }
-        if (cudaMemcpy(inputs3, inputs2, sizeof(size_t) * icm1, cudaMemcpyDeviceToHost)) {
-            throw std::exception();
-        }
-        cudaFree(nn2.inputs);
-        nn2.inputs = inputs3;
-        nn2.inputCount = icm1;
-        nodes[outputs[i]] = nn2;
-        delete[] inputs2;
     }
     delete[] inputs;
     delete[] outputs;
