aa05e7e78f706c2ce39bfc1e1f79e14249c0be73
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-06-30 22:04:51 -0400

    Corrected variable name usage

--- brendancuda_fields_instance2.cuh
+++ brendancuda_fields_instance2.cuh
@@ -11,7 +11,9 @@ namespace BrendanCUDA {
         struct FieldInstance2_CurrentInstance final {
             Fields::DField2<_T>& field;
             uint32_2* inputs;
+            size_t inputCount;
             uint32_2* outputs;
+            size_t outputCount;
             void* obj;
             void* objectRunner_sharedData;
             Random::AnyRNG<uint32_t> rng;
@@ -22,11 +24,12 @@ namespace BrendanCUDA {
         using fieldInstance2_objectRunner_t = void(*)(void* Object, DField2<_T> Field, void* SharedData);
         template <typename _T>
         using fieldInstance2_createField_t = DField2<_T>*(*)(void* SharedData);
-        template <typename _T>
         struct FieldInstance2_Construct_Settings final {
             Random::AnyRNG<uint32_t> rng;
             void* objectRunner_sharedData;
             void* createField_sharedData;
+            size_t inputCount;
+            size_t outputCount;
         };
         template <typename _T, fieldInstance2_createField_t<_T> _CreateField>
         void* FieldInstance2_Construct(void* Object, void* Settings);
@@ -39,26 +42,27 @@ namespace BrendanCUDA {
 
 template <typename _T, BrendanCUDA::Fields::fieldInstance2_createField_t<_T> _CreateField>
 void* BrendanCUDA::Fields::FieldInstance2_Construct(void* Object, void* Settings) {
-    FieldInstance2_Construct_Settings<_T> settings = *(FieldInstance2_Construct_Settings<_T>*)Settings;
+    FieldInstance2_Construct_Settings settings = *(FieldInstance2_Construct_Settings*)Settings;
     Random::AnyRNG<uint32_t>& rng = settings.rng;
 
     DField2<_T>& f = *_CreateField(settings.createField_sharedData);
-    details::FieldInstance2_CurrentInstance<_T>* p_rv = new details::FieldInstance2_CurrentInstance<_T>{ f, 0, 0, Object, settings.objectRunner_sharedData, rng };
-    uint32_2*& il = p_rv->inputs;
-    uint32_2*& ol = p_rv->outputs;
+    details::FieldInstance2_CurrentInstance<_T>* p_rv = new details::FieldInstance2_CurrentInstance<_T>{ f, 0, settings.inputCount, 0, settings.outputCount, Object, settings.objectRunner_sharedData, rng };
+    details::FieldInstance3_CurrentInstance<_T>& rv = *p_rv;
+    uint32_2*& il = rv.inputs;
+    uint32_2*& ol = rv.outputs;
 
     std::uniform_int_distribution<uint64_t> disU64(0, std::numeric_limits<uint64_t>::max());
 
     std::uniform_int_distribution<uint32_t> disX(0, f.LengthX() - 1);
     std::uniform_int_distribution<uint32_t> disY(0, f.LengthY() - 1);
 
-    il = new uint32_2[fieldOutputLength];
-    for (size_t i = 0; i < fieldOutputLength; ++i) {
+    il = new uint32_2[settings.inputCount];
+    for (size_t i = 0; i < settings.inputCount; ++i) {
         il[i] = uint32_2{ disX(rng), disY(rng) };
     }
 
-    ol = new uint32_2[fieldOutputLength];
-    for (size_t i = 0; i < fieldOutputLength; ++i) {
+    ol = new uint32_2[settings.outputCount];
+    for (size_t i = 0; i < settings.outputCount; ++i) {
         ol[i] = uint32_2{ disX(rng), disY(rng) };
     }
 
@@ -67,7 +71,7 @@ void* BrendanCUDA::Fields::FieldInstance2_Construct(void* Object, void* Settings
 template <typename _T, BrendanCUDA::Fields::fieldInstance2_objectRunner_t<_T> _ObjRunner>
 _T* BrendanCUDA::Fields::FieldInstance2_Iterate(void* CurrentInstance, _T* Inputs) {
     details::FieldInstance2_CurrentInstance<_T> c = *(details::FieldInstance2_CurrentInstance<_T>*)CurrentInstance;
-    DField2<uint64_t>& f = c.field;
+    DField2<_T>& f = c.field;
     uint32_2* il = c.inputs;
     uint32_2* ol = c.outputs;
 
@@ -77,8 +81,8 @@ _T* BrendanCUDA::Fields::FieldInstance2_Iterate(void* CurrentInstance, _T* Input
         }
     }
     _ObjRunner(c.obj, f, c.objectRunner_sharedData);
-    _T* opts = new _T[fieldOutputLength];
-    for (size_t i = 0; i < fieldOutputLength; ++i) {
+    _T* opts = new _T[c.outputCount];
+    for (size_t i = 0; i < c.outputCount; ++i) {
         opts[i] = f.FFront().GetValueAt(ol[i]);
     }
     return opts;
--- brendancuda_fields_instance3.cuh
+++ brendancuda_fields_instance3.cuh
@@ -11,7 +11,9 @@ namespace BrendanCUDA {
         struct FieldInstance3_CurrentInstance final {
             Fields::DField3<_T>& field;
             uint32_3* inputs;
+            size_t inputCount;
             uint32_3* outputs;
+            size_t outputCount;
             void* obj;
             void* objectRunner_sharedData;
             Random::AnyRNG<uint32_t> rng;
@@ -22,11 +24,12 @@ namespace BrendanCUDA {
         using fieldInstance3_objectRunner_t = void(*)(void* Object, DField3<_T> Field, void* SharedData);
         template <typename _T>
         using fieldInstance3_createField_t = DField3<_T>*(*)(void* SharedData);
-        template <typename _T>
         struct FieldInstance3_Construct_Settings final {
             Random::AnyRNG<uint32_t> rng;
             void* objectRunner_sharedData;
             void* createField_sharedData;
+            size_t inputCount;
+            size_t outputCount;
         };
         template <typename _T, fieldInstance3_createField_t<_T> _CreateField>
         void* FieldInstance3_Construct(void* Object, void* Settings);
@@ -39,13 +42,14 @@ namespace BrendanCUDA {
 
 template <typename _T, BrendanCUDA::Fields::fieldInstance3_createField_t<_T> _CreateField>
 void* BrendanCUDA::Fields::FieldInstance3_Construct(void* Object, void* Settings) {
-    FieldInstance3_Construct_Settings<_T> settings = *(FieldInstance3_Construct_Settings<_T>*)Settings;
+    FieldInstance3_Construct_Settings settings = *(FieldInstance3_Construct_Settings*)Settings;
     Random::AnyRNG<uint32_t>& rng = settings.rng;
 
     DField3<_T>& f = *_CreateField(settings.createField_sharedData);
-    details::FieldInstance3_CurrentInstance<_T>* p_rv = new details::FieldInstance3_CurrentInstance<_T>{ f, 0, 0, Object, settings.objectRunner_sharedData, rng };
-    uint32_3*& il = p_rv->inputs;
-    uint32_3*& ol = p_rv->outputs;
+    details::FieldInstance3_CurrentInstance<_T>* p_rv = new details::FieldInstance3_CurrentInstance<_T>{ f, 0, settings.inputCount, 0, settings.outputCount, Object, settings.objectRunner_sharedData, rng };
+    details::FieldInstance3_CurrentInstance<_T>& rv = *p_rv;
+    uint32_3*& il = rv.inputs;
+    uint32_3*& ol = rv.outputs;
 
     std::uniform_int_distribution<uint64_t> disU64(0, std::numeric_limits<uint64_t>::max());
 
@@ -53,13 +57,13 @@ void* BrendanCUDA::Fields::FieldInstance3_Construct(void* Object, void* Settings
     std::uniform_int_distribution<uint32_t> disY(0, f.LengthY() - 1);
     std::uniform_int_distribution<uint32_t> disZ(0, f.LengthZ() - 1);
 
-    il = new uint32_3[fieldOutputLength];
-    for (size_t i = 0; i < fieldOutputLength; ++i) {
+    il = new uint32_3[settings.inputCount];
+    for (size_t i = 0; i < settings.inputCount; ++i) {
         il[i] = uint32_3{ disX(rng), disY(rng), disZ(rng) };
     }
 
-    ol = new uint32_3[fieldOutputLength];
-    for (size_t i = 0; i < fieldOutputLength; ++i) {
+    ol = new uint32_3[settings.outputCount];
+    for (size_t i = 0; i < settings.outputCount; ++i) {
         ol[i] = uint32_3{ disX(rng), disY(rng), disZ(rng) };
     }
 
@@ -68,18 +72,18 @@ void* BrendanCUDA::Fields::FieldInstance3_Construct(void* Object, void* Settings
 template <typename _T, BrendanCUDA::Fields::fieldInstance3_objectRunner_t<_T> _ObjRunner>
 _T* BrendanCUDA::Fields::FieldInstance3_Iterate(void* CurrentInstance, _T* Inputs) {
     details::FieldInstance3_CurrentInstance<_T> c = *(details::FieldInstance3_CurrentInstance<_T>*)CurrentInstance;
-    DField3<uint64_t>& f = c.field;
+    DField3<_T>& f = c.field;
     uint32_3* il = c.inputs;
     uint32_3* ol = c.outputs;
 
     if (Inputs) {
-        for (size_t i = 0; i < fieldInputLength; ++i) {
+        for (size_t i = 0; i < c.inputCount; ++i) {
             f.FFront().SetValueAt(il[i], Inputs[i]);
         }
     }
     _ObjRunner(c.obj, f, c.objectRunner_sharedData);
-    _T* opts = new _T[fieldOutputLength];
-    for (size_t i = 0; i < fieldOutputLength; ++i) {
+    _T* opts = new _T[c.outputCount];
+    for (size_t i = 0; i < c.outputCount; ++i) {
         opts[i] = f.FFront().GetValueAt(ol[i]);
     }
     return opts;
