05acda1e2364b58748df41d2970abf4f234acaf7
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-21 10:57:57 -0400

    Improved BrendanCUDA::Math::sqrt

--- brendancuda_mathfuncs.h
+++ brendancuda_mathfuncs.h
@@ -13,14 +13,7 @@ namespace BrendanCUDA {
         __host__ __device__ __forceinline _T clamp(_T value, _T lower, _T upper);
     }
 }
-template <>
-__host__ __device__ __forceinline float BrendanCUDA::Math::sqrt<float>(float value) {
-    return std::sqrt(value);
-}
-template <>
-__host__ __device__ __forceinline double BrendanCUDA::Math::sqrt<double>(double value) {
-    return std::sqrt(value);
-}
+
 template <>
 __host__ __device__ __forceinline int32_t BrendanCUDA::Math::sqrt<int32_t>(int32_t value) {
     if (value < 0) {
@@ -67,22 +60,6 @@ __host__ __device__ __forceinline uint32_t BrendanCUDA::Math::sqrt<uint32_t>(uin
     return lower;
 }
 template <>
-__host__ __device__ __forceinline int8_t BrendanCUDA::Math::sqrt<int8_t>(int8_t value) {
-    return (int8_t)sqrt((int32_t)value);
-}
-template <>
-__host__ __device__ __forceinline uint8_t BrendanCUDA::Math::sqrt<uint8_t>(uint8_t value) {
-    return (uint8_t)sqrt((uint32_t)value);
-}
-template <>
-__host__ __device__ __forceinline int16_t BrendanCUDA::Math::sqrt<int16_t>(int16_t value) {
-    return (int16_t)sqrt((int32_t)value);
-}
-template <>
-__host__ __device__ __forceinline uint16_t BrendanCUDA::Math::sqrt<uint16_t>(uint16_t value) {
-    return (uint16_t)sqrt((uint32_t)value);
-}
-template <>
 __host__ __device__ __forceinline int64_t BrendanCUDA::Math::sqrt<int64_t>(int64_t value) {
     if (value < 0) {
         return -1;
@@ -126,6 +103,15 @@ __host__ __device__ __forceinline uint64_t BrendanCUDA::Math::sqrt<uint64_t>(uin
     } while (upper > (lower + 1));
 
     return lower;
+}
+template <typename _T>
+__host__ __device__ __forceinline _T BrendanCUDA::Math::sqrt(_T value) {
+    if constexpr (std::signed_integral<_T>)
+        return (_T)sqrt((int32_t)value);
+    else if constexpr (std::unsigned_integral<_T>)
+        return (_T)sqrt((uint32_t)value);
+    else
+        return std::sqrt(value);
 }
 
 template <typename _T>
