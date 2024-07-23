3567d2261d0774eabab6217dc8f7dadec1006a15
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-22 22:07:24 -0400

    Added generalized function for IO type conversion

--- brendancuda_fields_instance2.h
+++ brendancuda_fields_instance2.h
@@ -30,6 +30,12 @@ namespace BrendanCUDA {
             size_t inputCount;
             size_t outputCount;
         };
+
+        template <typename _TFieldValue, typename _TOutput>
+        using fieldInstance2_getOutput_t = _TOutput(*)(const _TFieldValue& FieldValue);
+        template <typename _TFieldValue, typename _TInput>
+        using fieldInstance2_assignInput_t = void(*)(_TFieldValue& FieldValue, const _TInput& InputValue);
+
         template <typename _T, fieldInstance2_createField_t<_T> _CreateField>
         void* FieldInstance2_Construct(void* Object, void* Settings);
         template <typename _T, fieldInstance2_objectRunner_t<_T> _ObjectRunner>
@@ -38,6 +44,11 @@ namespace BrendanCUDA {
         void FieldInstance2_Destruct(void* CurrentInstance);
         template <typename _T, fieldInstance2_createField_t<_T> _CreateField, fieldInstance2_objectRunner_t<_T> _ObjectRunner>
         AI::Evolution::Evaluation::Output::InstanceFunctions<_T*, _T*> FieldInstance2();
+
+        template <typename _TFieldValue, typename _TInput, typename _TOutput, fieldInstance2_assignInput_t<_TFieldValue, _TInput> _AssignInput, fieldInstance2_getOutput_t<_TFieldValue, _TOutput> _GetOutput, fieldInstance2_objectRunner_t<_TFieldValue> _ObjectRunner>
+        _TOutput* FieldInstance2_Iterate(void* CurrentInstance, _TInput* Inputs);
+        template <typename _TFieldValue, typename _TInput, typename _TOutput, fieldInstance2_createField_t<_TFieldValue> _CreateField, fieldInstance2_assignInput_t<_TFieldValue, _TInput> _AssignInput, fieldInstance2_getOutput_t<_TFieldValue, _TOutput> _GetOutput, fieldInstance2_objectRunner_t<_TFieldValue> _ObjectRunner>
+        AI::Evolution::Evaluation::Output::InstanceFunctions<_TInput*, _TOutput*> FieldInstance2();
     }
 }
 
@@ -105,5 +116,38 @@ BrendanCUDA::AI::Evolution::Evaluation::Output::InstanceFunctions<_T*, _T*> Bren
     ifs.constructInstance = FieldInstance2_Construct<_T, _CreateField>;
     ifs.iterateInstance = FieldInstance2_Iterate<_T, _ObjectRunner>;
     ifs.destructInstance = FieldInstance2_Destruct<_T>;
+    return ifs;
+}
+
+template <typename _TFieldValue, typename _TInput, typename _TOutput, BrendanCUDA::Fields::fieldInstance2_assignInput_t<_TFieldValue, _TInput> _AssignInput, BrendanCUDA::Fields::fieldInstance2_getOutput_t<_TFieldValue, _TOutput> _GetOutput, BrendanCUDA::Fields::fieldInstance2_objectRunner_t<_TFieldValue> _ObjectRunner>
+_TOutput* BrendanCUDA::Fields::FieldInstance2_Iterate(void* CurrentInstance, _TInput* Inputs) {
+    details::FieldInstance2_CurrentInstance<_TFieldValue> c = *(details::FieldInstance2_CurrentInstance<_TFieldValue>*)CurrentInstance;
+    DField2<_TFieldValue>& f = c.field;
+    ArrayV<uint32_2> il = c.inputs;
+    ArrayV<uint32_2> ol = c.outputs;
+
+    if (Inputs) {
+        for (size_t i = 0; i < il.size; ++i) {
+            auto& idx = il[i];
+            _TFieldValue fieldValue = f.FFront().GetValueAt(idx);
+            _AssignInput(fieldValue, Inputs[i]);
+            f.FFront().SetValueAt(idx, fieldValue);
+        }
+    }
+    _ObjectRunner(c.obj, f, c.objectRunner_sharedData);
+    _TOutput* opts = new _TOutput[ol.size];
+    for (size_t i = 0; i < ol.size; ++i) {
+        _TFieldValue fieldValue = f.FFront().GetValueAt(ol[i]);
+        opts[i] = _GetOutput(fieldValue);
+    }
+    return opts;
+}
+
+template <typename _TFieldValue, typename _TInput, typename _TOutput, BrendanCUDA::Fields::fieldInstance2_createField_t<_TFieldValue> _CreateField, BrendanCUDA::Fields::fieldInstance2_assignInput_t<_TFieldValue, _TInput> _AssignInput, BrendanCUDA::Fields::fieldInstance2_getOutput_t<_TFieldValue, _TOutput> _GetOutput, BrendanCUDA::Fields::fieldInstance2_objectRunner_t<_TFieldValue> _ObjectRunner>
+BrendanCUDA::AI::Evolution::Evaluation::Output::InstanceFunctions<_TInput*, _TOutput*> BrendanCUDA::Fields::FieldInstance2() {
+    AI::Evolution::Evaluation::Output::InstanceFunctions<_TInput*, _TOutput*> ifs;
+    ifs.constructInstance = FieldInstance2_Construct<_TFieldValue, _CreateField>;
+    ifs.iterateInstance = FieldInstance2_Iterate<_TFieldValue, _TInput, _TOutput, _AssignInput, _GetOutput, _ObjectRunner>;
+    ifs.destructInstance = FieldInstance2_Destruct<_TFieldValue>;
     return ifs;
 }
--- brendancuda_fields_instance3.h
+++ brendancuda_fields_instance3.h
@@ -30,6 +30,12 @@ namespace BrendanCUDA {
             size_t inputCount;
             size_t outputCount;
         };
+
+        template <typename _TFieldValue, typename _TOutput>
+        using fieldInstance3_getOutput_t = _TOutput(*)(const _TFieldValue& FieldValue);
+        template <typename _TFieldValue, typename _TInput>
+        using fieldInstance3_assignInput_t = void(*)(_TFieldValue& FieldValue, const _TInput& InputValue);
+
         template <typename _T, fieldInstance3_createField_t<_T> _CreateField>
         void* FieldInstance3_Construct(void* Object, void* Settings);
         template <typename _T, fieldInstance3_objectRunner_t<_T> _ObjectRunner>
@@ -38,6 +44,11 @@ namespace BrendanCUDA {
         void FieldInstance3_Destruct(void* CurrentInstance);
         template <typename _T, fieldInstance3_createField_t<_T> _CreateField, fieldInstance3_objectRunner_t<_T> _ObjectRunner>
         AI::Evolution::Evaluation::Output::InstanceFunctions<_T*, _T*> FieldInstance3();
+
+        template <typename _TFieldValue, typename _TInput, typename _TOutput, fieldInstance3_assignInput_t<_TFieldValue, _TInput> _AssignInput, fieldInstance3_getOutput_t<_TFieldValue, _TOutput> _GetOutput, fieldInstance3_objectRunner_t<_TFieldValue> _ObjectRunner>
+        _TOutput* FieldInstance3_Iterate(void* CurrentInstance, _TInput* Inputs);
+        template <typename _TFieldValue, typename _TInput, typename _TOutput, fieldInstance3_createField_t<_TFieldValue> _CreateField, fieldInstance3_assignInput_t<_TFieldValue, _TInput> _AssignInput, fieldInstance3_getOutput_t<_TFieldValue, _TOutput> _GetOutput, fieldInstance3_objectRunner_t<_TFieldValue> _ObjectRunner>
+        AI::Evolution::Evaluation::Output::InstanceFunctions<_TInput*, _TOutput*> FieldInstance3();
     }
 }
 
@@ -106,5 +117,38 @@ BrendanCUDA::AI::Evolution::Evaluation::Output::InstanceFunctions<_T*, _T*> Bren
     ifs.constructInstance = FieldInstance3_Construct<_T, _CreateField>;
     ifs.iterateInstance = FieldInstance3_Iterate<_T, _ObjectRunner>;
     ifs.destructInstance = FieldInstance3_Destruct<_T>;
+    return ifs;
+}
+
+template <typename _TFieldValue, typename _TInput, typename _TOutput, BrendanCUDA::Fields::fieldInstance3_assignInput_t<_TFieldValue, _TInput> _AssignInput, BrendanCUDA::Fields::fieldInstance3_getOutput_t<_TFieldValue, _TOutput> _GetOutput, BrendanCUDA::Fields::fieldInstance3_objectRunner_t<_TFieldValue> _ObjectRunner>
+_TOutput* BrendanCUDA::Fields::FieldInstance3_Iterate(void* CurrentInstance, _TInput* Inputs) {
+    details::FieldInstance3_CurrentInstance<_TFieldValue> c = *(details::FieldInstance3_CurrentInstance<_TFieldValue>*)CurrentInstance;
+    DField3<_TFieldValue>& f = c.field;
+    ArrayV<uint32_3> il = c.inputs;
+    ArrayV<uint32_3> ol = c.outputs;
+
+    if (Inputs) {
+        for (size_t i = 0; i < il.size; ++i) {
+            auto& idx = il[i];
+            _TFieldValue fieldValue = f.FFront().GetValueAt(idx);
+            _AssignInput(fieldValue, Inputs[i]);
+            f.FFront().SetValueAt(idx, fieldValue);
+        }
+    }
+    _ObjectRunner(c.obj, f, c.objectRunner_sharedData);
+    _TOutput* opts = new _TOutput[ol.size];
+    for (size_t i = 0; i < ol.size; ++i) {
+        _TFieldValue fieldValue = f.FFront().GetValueAt(ol[i]);
+        opts[i] = _GetOutput(fieldValue);
+    }
+    return opts;
+}
+
+template <typename _TFieldValue, typename _TInput, typename _TOutput, BrendanCUDA::Fields::fieldInstance3_createField_t<_TFieldValue> _CreateField, BrendanCUDA::Fields::fieldInstance3_assignInput_t<_TFieldValue, _TInput> _AssignInput, BrendanCUDA::Fields::fieldInstance3_getOutput_t<_TFieldValue, _TOutput> _GetOutput, BrendanCUDA::Fields::fieldInstance3_objectRunner_t<_TFieldValue> _ObjectRunner>
+BrendanCUDA::AI::Evolution::Evaluation::Output::InstanceFunctions<_TInput*, _TOutput*> BrendanCUDA::Fields::FieldInstance3() {
+    AI::Evolution::Evaluation::Output::InstanceFunctions<_TInput*, _TOutput*> ifs;
+    ifs.constructInstance = FieldInstance3_Construct<_TFieldValue, _CreateField>;
+    ifs.iterateInstance = FieldInstance3_Iterate<_TFieldValue, _TInput, _TOutput, _AssignInput, _GetOutput, _ObjectRunner>;
+    ifs.destructInstance = FieldInstance3_Destruct<_TFieldValue>;
     return ifs;
 }
