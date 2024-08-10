6dab0a2695c89e1864237792a4784346195d468f
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-08-10 11:17:13 -0400

    Inlined Get64Bits

--- brendancuda_rand_bits.h
+++ brendancuda_rand_bits.h
@@ -7,6 +7,25 @@
 
 namespace BrendanCUDA {
     namespace Random {
-        __host__ __device__ uint64_t Get64Bits(uint32_t ProbabilityOf1, AnyRNG<uint64_t> RNG);
+        __host__ __device__ __forceinline uint64_t Get64Bits(uint32_t ProbabilityOf1, AnyRNG<uint64_t> RNG);
     }
+}
+
+
+__host__ __device__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32_t ProbabilityOf1, AnyRNG<uint64_t> RNG) {
+    uint32_t ct = BrendanCUDA::Binary::CountBitsB(ProbabilityOf1);
+    if (!ct) {
+        return 0;
+    }
+    uint32_t lb = 1u << 31 >> (ct - 1);
+    uint64_t cr = RNG();
+    for (uint32_t i = 1ui64 << 31; i > lb; i >>= 1) {
+        if (ProbabilityOf1 & i) {
+            cr |= RNG();
+        }
+        else {
+            cr &= RNG();
+        }
+    }
+    return cr;
 }
