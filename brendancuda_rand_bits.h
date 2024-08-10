cfb5b1084070559078945cd1e4c6ed014d3538e7
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-08-10 11:55:14 -0400

    Made RNG for Get64Bits templated

--- brendancuda_rand_bits.h
+++ brendancuda_rand_bits.h
@@ -7,24 +7,26 @@
 
 namespace BrendanCUDA {
     namespace Random {
-        __host__ __device__ __forceinline uint64_t Get64Bits(uint32_t ProbabilityOf1, AnyRNG<uint64_t> RNG);
+        template <std::uniform_random_bit_generator _TRNG>
+        __host__ __device__ __forceinline uint64_t Get64Bits(uint32_t ProbabilityOf1, _TRNG RNG);
     }
 }
 
-
-__host__ __device__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32_t ProbabilityOf1, AnyRNG<uint64_t> RNG) {
+template <std::uniform_random_bit_generator _TRNG>
+__host__ __device__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32_t ProbabilityOf1, _TRNG RNG) {
     uint32_t ct = BrendanCUDA::Binary::CountBitsB(ProbabilityOf1);
     if (!ct) {
         return 0;
     }
+    std::uniform_int_distribution<uint64_t> dis64(0);
     uint32_t lb = 1u << 31 >> (ct - 1);
-    uint64_t cr = RNG();
+    uint64_t cr = dis64(RNG);
     for (uint32_t i = 1ui64 << 31; i > lb; i >>= 1) {
         if (ProbabilityOf1 & i) {
-            cr |= RNG();
+            cr |= dis64(RNG);
         }
         else {
-            cr &= RNG();
+            cr &= dis64(RNG);
         }
     }
     return cr;
