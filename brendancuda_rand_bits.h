632147b0996db542711c758a19a7d3c462db8717
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-08-10 15:00:40 -0400

    Made Get64Bits CURAND-compatible

--- brendancuda_rand_bits.h
+++ brendancuda_rand_bits.h
@@ -2,18 +2,42 @@
 
 #include <cuda_runtime.h>
 #include <cstdint>
+#include <curand_kernel.h>
 #include "brendancuda_rand_anyrng.h"
 #include "brendancuda_binary_basic.h"
 
 namespace BrendanCUDA {
     namespace Random {
         template <std::uniform_random_bit_generator _TRNG>
+        __host__ __device__ __forceinline uint32_t Get32Bits(uint32_t ProbabilityOf1, _TRNG RNG);
+        template <std::uniform_random_bit_generator _TRNG>
         __host__ __device__ __forceinline uint64_t Get64Bits(uint32_t ProbabilityOf1, _TRNG RNG);
+        __host__ __device__ __forceinline uint32_t Get32Bits(uint32_t ProbabilityOf1, curandState RNG);
+        __host__ __device__ __forceinline uint64_t Get64Bits(uint32_t ProbabilityOf1, curandState RNG);
     }
 }
 
 template <std::uniform_random_bit_generator _TRNG>
-__host__ __device__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32_t ProbabilityOf1, _TRNG RNG) {
+__host__ __forceinline uint32_t BrendanCUDA::Random::Get32Bits(uint32_t ProbabilityOf1, _TRNG RNG) {
+    uint32_t ct = BrendanCUDA::Binary::CountBitsB(ProbabilityOf1);
+    if (!ct) {
+        return 0;
+    }
+    std::uniform_int_distribution<uint32_t> dis32(0);
+    uint32_t lb = 1u << 31 >> (ct - 1);
+    uint32_t cr = dis32(RNG);
+    for (uint32_t i = 1ui64 << 31; i > lb; i >>= 1) {
+        if (ProbabilityOf1 & i) {
+            cr |= dis32(RNG);
+        }
+        else {
+            cr &= dis32(RNG);
+        }
+    }
+    return cr;
+}
+template <std::uniform_random_bit_generator _TRNG>
+__host__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32_t ProbabilityOf1, _TRNG RNG) {
     uint32_t ct = BrendanCUDA::Binary::CountBitsB(ProbabilityOf1);
     if (!ct) {
         return 0;
@@ -27,6 +51,40 @@ __host__ __device__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32
         }
         else {
             cr &= dis64(RNG);
+        }
+    }
+    return cr;
+}
+__host__ __device__ __forceinline uint32_t BrendanCUDA::Random::Get32Bits(uint32_t ProbabilityOf1, curandState RNG) {
+    uint32_t ct = BrendanCUDA::Binary::CountBitsB(ProbabilityOf1);
+    if (!ct) {
+        return 0;
+    }
+    uint32_t lb = 1u << 31 >> (ct - 1);
+    uint32_t cr = curand(&RNG);
+    for (uint32_t i = 1ui64 << 31; i > lb; i >>= 1) {
+        if (ProbabilityOf1 & i) {
+            cr |= curand(&RNG);
+        }
+        else {
+            cr &= curand(&RNG);
+        }
+    }
+    return cr;
+}
+__host__ __device__ __forceinline uint64_t BrendanCUDA::Random::Get64Bits(uint32_t ProbabilityOf1, curandState RNG) {
+    uint32_t ct = BrendanCUDA::Binary::CountBitsB(ProbabilityOf1);
+    if (!ct) {
+        return 0;
+    }
+    uint32_t lb = 1u << 31 >> (ct - 1);
+    uint64_t cr = ((uint64_t)curand(&RNG) << 32) | curand(&RNG);
+    for (uint32_t i = 1ui64 << 31; i > lb; i >>= 1) {
+        if (ProbabilityOf1 & i) {
+            cr |= ((uint64_t)curand(&RNG) << 32) | curand(&RNG);
+        }
+        else {
+            cr &= ((uint64_t)curand(&RNG) << 32) | curand(&RNG);
         }
     }
     return cr;
