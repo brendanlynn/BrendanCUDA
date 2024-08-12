ebfc84aa20306fddf41109afb6ae4f018d5a4168
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-08-11 21:26:35 -0400

    Removed misplaced functions from brendancuda_ai.*

--- brendancuda_ai.h
+++ brendancuda_ai.h
@@ -32,66 +32,6 @@ namespace BrendanCUDA {
         template <typename _T>
         __host__ __device__ _T Sigmoid(_T Value);
 
-        //Makes a small, random adjustment in the range of [-Scalar, +Scalar] to each of the values of Array (Array being a pointer to VRAM).
-        __host__ __device__ void RandomizeArray(Span<float> Array, float Scalar, Random::AnyRNG<uint64_t> RNG);
-        //Makes a small, random adjustment in the range of [-Scalar, +Scalar] to each of the values of Array (Array being a pointer to VRAM), clamping the final value in the range of [LowerBound, UpperBound], even if it wasn't in that range to start.
-        __host__ __device__ void RandomizeArray(Span<float> Array, float Scalar, float LowerBound, float UpperBound, Random::AnyRNG<uint64_t> RNG);
-
-        //Makes a small, random adjustment in the range of [-Scalar, +Scalar] to each of the values of Array (Array being a pointer to VRAM).
-        __host__ __device__ void RandomizeArray(Span<double> Array, double Scalar, Random::AnyRNG<uint64_t> RNG);
-        //Makes a small, random adjustment in the range of [-Scalar, +Scalar] to each of the values of Array (Array being a pointer to VRAM), clamping the final value in the range of [LowerBound, UpperBound], even if it wasn't in that range to start.
-        __host__ __device__ void RandomizeArray(Span<double> Array, double Scalar, double LowerBound, double UpperBound, Random::AnyRNG<uint64_t> RNG);
-
-        //Flips bits in Array (Array being a pointer to VRAM), the probability of each bit being flipped a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void RandomizeArray(Span<uint64_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-        //Flips bits in Array (Array being a pointer to VRAM), the probability of each bit being flipped a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void RandomizeArray(Span<uint32_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-        //Flips bits in Array (Array being a pointer to VRAM), the probability of each bit being flipped a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void RandomizeArray(Span<uint16_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-        //Flips bits in Array (Array being a pointer to VRAM), the probability of each bit being flipped a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void RandomizeArray(Span<uint8_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-
-        //Fills Array (Array being a pointer to VRAM) with random numbers uniformly distributed in the range of [0, 1].
-        __host__ __device__ void InitRandomArray(Span<float> Array, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random numbers uniformly distributed in the range of [LowerBound, UpperBound].
-        __host__ __device__ void InitRandomArray(Span<float> Array, float LowerBound, float UpperBound, Random::AnyRNG<uint64_t> RNG);
-
-        //Fills Array (Array being a pointer to VRAM) with random numbers uniformly distributed in the range of [0, 1].
-        __host__ __device__ void InitRandomArray(Span<double> Array, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random numbers uniformly distributed in the range of [LowerBound, UpperBound].
-        __host__ __device__ void InitRandomArray(Span<double> Array, double LowerBound, double UpperBound, Random::AnyRNG<uint64_t> RNG);
-
-        //Fills Array (Array being a pointer to VRAM) with random bits.
-        __host__ __device__ void InitRandomArray(Span<uint64_t> Array, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random bits.
-        __host__ __device__ void InitRandomArray(Span<uint32_t> Array, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random bits.
-        __host__ __device__ void InitRandomArray(Span<uint16_t> Array, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random bits.
-        __host__ __device__ void InitRandomArray(Span<uint8_t> Array, Random::AnyRNG<uint64_t> RNG);
-
-        //Fills Array (Array being a pointer to VRAM) with random bits, each bit having their probability of being 1 as a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void InitRandomArray(Span<uint64_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random bits, each bit having their probability of being 1 as a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void InitRandomArray(Span<uint32_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random bits, each bit having their probability of being 1 as a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void InitRandomArray(Span<uint16_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-        //Fills Array (Array being a pointer to VRAM) with random bits, each bit having their probability of being 1 as a function of ProbabilityOf1, where the binary digits of ProbabilityOf1 are binary digits after the decimal point of a base-2 number.
-        __host__ __device__ void InitRandomArray(Span<uint8_t> Array, uint32_t ProbabilityOf1, Random::AnyRNG<uint64_t> RNG);
-
-        //Fills Array (Array being a pointer to VRAM) with all 0s.
-        __host__ __device__ void InitZeroArray(Span<float> Array);
-        //Fills Array (Array being a pointer to VRAM) with all 0s.
-        __host__ __device__ void InitZeroArray(Span<double> Array);
-        //Fills Array (Array being a pointer to VRAM) with all 0s.
-        __host__ __device__ void InitZeroArray(Span<uint64_t> Array);
-        //Fills Array (Array being a pointer to VRAM) with all 0s.
-        __host__ __device__ void InitZeroArray(Span<uint32_t> Array);
-        //Fills Array (Array being a pointer to VRAM) with all 0s.
-        __host__ __device__ void InitZeroArray(Span<uint16_t> Array);
-        //Fills Array (Array being a pointer to VRAM) with all 0s.
-        __host__ __device__ void InitZeroArray(Span<uint8_t> Array);
-
         //Converts an array of floats (Floats) to bools, writing the result to Bools. Each bool is true if its corresponding float is greater than Split, and false otherwise. MemoryOnHost specifies whether or not the pointers point to memory on the RAM or the VRAM (true being the RAM, false being the VRAM).
         __host__ void CopyFloatsToBools(float* Floats, bool* Bools, size_t Length, float Split, bool MemoryOnHost);
         //Converts an array of doubles (Doubles) to bools, writing the result to Bools. Each bool is true if its corresponding double is greater than Split, and false otherwise. MemoryOnHost specifies whether or not the pointers point to memory on the RAM or the VRAM (true being the RAM, false being the VRAM).
