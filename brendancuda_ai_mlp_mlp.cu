a522cceffc43e29e9e38bf32a6335f730d60f026
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-08-15 19:33:13 -0400

    Added possibilities for template parameters

--- brendancuda_ai_mlp_mlp.cu
+++ brendancuda_ai_mlp_mlp.cu
@@ -19,3 +19,6 @@ __host__ __device__ void BrendanCUDA::details::RunActivationFunctionOnArray(Span
     runActivationFunctionOnArrayKernel<<<Array.size, 1>>>(Array.ptr, ActivationFunction);
 #endif
 }
+
+template void BrendanCUDA::details::RunActivationFunctionOnArray<float>(Span<float>, AI::activationFunction_t<float>);
+template void BrendanCUDA::details::RunActivationFunctionOnArray<double>(Span<double>, AI::activationFunction_t<double>);
