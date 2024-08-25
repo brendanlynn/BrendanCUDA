8c2ed7b41a817006c4685925d5431b6ba92d61b8
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-08-25 12:06:27 -0400

    Added ...curandkernelgens.h

---
+++ brendancuda_curandkernelgens.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <concepts>
+#include <curand_kernel.h>
+
+namespace BrendanCUDA {
+    template <typename _T>
+    concept KernelCurandState =
+        std::same_as<_T, curandStateMRG32k3a_t> ||
+        std::same_as<_T, curandStateMtgp32_t> ||
+        std::same_as<_T, curandStatePhilox4_32_10_t> ||
+        std::same_as<_T, curandStateScrambledSobol32_t> ||
+        std::same_as<_T, curandStateScrambledSobol64_t> ||
+        std::same_as<_T, curandStateSobol32_t> ||
+        std::same_as<_T, curandStateSobol64_t> ||
+        std::same_as<_T, curandStateXORWOW_t>;
+}
