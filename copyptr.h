87725319f693523483bed94cfde13133c4c9bfb8
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-09-13 12:40:09 -0400

    Fixed errors and removed redundancies

--- copyptr.h
+++ copyptr.h
@@ -11,35 +11,27 @@ namespace BrendanCUDA {
         using element_t = _T;
         static constexpr bool copyable = std::copy_constructible<_T>;
 
-        explicit CopyPtr(std::unique_ptr<_T> UniPtr)
+        __forceinline explicit CopyPtr(std::unique_ptr<_T> UniPtr)
             : ptr(std::move(UniPtr)) { }
-        CopyPtr(const CopyPtr<_T>& Other) requires copyable
-            : ptr(Other.ptr ? std::make_unique<_T>(*Other.ptr) : std::unique_ptr<_T>(0)) { }
-        CopyPtr(CopyPtr<_T>&& Other)
+        __forceinline CopyPtr(const CopyPtr<_T>& Other) requires copyable
+            : ptr(Other.ptr ? std::make_unique<_T>(*Other.ptr) : std::unique_ptr<_T>(nullptr)) { }
+        __forceinline CopyPtr(CopyPtr<_T>&& Other)
             : ptr(std::move(Other.ptr)) { }
-        CopyPtr(_T Val)
-            : ptr(std::make_unique<_T>(Val)) { }
+        __forceinline explicit CopyPtr(element_t* Val)
+            : ptr(Val) { }
+        __forceinline explicit CopyPtr(std::nullptr_t)
+            : ptr(nullptr) { }
 
-        CopyPtr<_T>& operator=(CopyPtr<_T> Other) {
+        __forceinline CopyPtr<_T>& operator=(CopyPtr<_T> Other) {
             std::swap(ptr, Other.ptr);
         }
 
-        _T* Get() const {
+        __forceinline _T* Get() const {
             return (_T*)ptr.get();
         }
 
-        _T* operator->() const {
+        __forceinline operator _T*() const {
             return ptr.get();
-        }
-        _T& operator*() const {
-            return *ptr;
-        }
-        _T& operator[](size_t Idx) {
-            return ptr[Idx];
-        }
-
-        operator _T*() const {
-            return ptr;
         }
     };
 }
