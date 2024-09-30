42617c1822750ac8c31162eb5d68a519f98c01d1
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-09-30 19:07:46 -0400

    Fixed `output` param names in `copyblock.h`

--- copyblock.h
+++ copyblock.h
@@ -23,20 +23,20 @@ namespace bcuda {
         __host__ __device__ ArrayV<Landmark> GetLandmarksInDirection(uint32_t InputLength, uint32_t OutputLength, uint32_t RangeLength, uint32_t InputIndex, uint32_t OutputIndex);
     }
     template <typename _T, size_t _VectorLength, bool _InputOnHost, bool _OutputOnHost, bool _Wrap = false>
-    __host__ static void CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
+    __host__ static void CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
 #ifdef __CUDACC__
     template <typename _T, size_t _VectorLength, bool _Wrap = false>
-    __device__ static void CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
+    __device__ static void CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
 #endif
 
     template <typename _T, size_t _VectorLength, copyValFunc_t<_T> _CopyValFunc, bool _Wrap = false>
-    __host__ __device__ static void CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
+    __host__ __device__ static void CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
     template <typename _T, size_t _VectorLength, copyArrFunc_t<_T> _CopyArrFunc, bool _Wrap = false>
-    __host__ __device__ static void CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
+    __host__ __device__ static void CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates);
 }
 
 template <typename _T, size_t _VectorLength, bool _InputOnHost, bool _OutputOnHost, bool _Wrap>
-__host__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
+__host__ void bcuda::CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
     using vector_t = FixedVector<uint32_t, _VectorLength>;
     
     if constexpr (_Wrap) {
@@ -56,7 +56,7 @@ __host__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<ui
                 rangeInOutputCoordinates[j] = landmark.outputIndex;
             }
 
-            CopyBlock<_T, _VectorLength, _InputOnHost, _OutputOnHost, false>(Input, output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
+            CopyBlock<_T, _VectorLength, _InputOnHost, _OutputOnHost, false>(Input, Output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
             
             bool toBreak = true;
             for (size_t j = 0; j < _VectorLength; ++j) {
@@ -73,11 +73,11 @@ __host__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<ui
     else {
         if constexpr (_VectorLength == 1) {
             if constexpr (_InputOnHost)
-                if constexpr (_OutputOnHost) memcpy(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x);
-                else cudaMemcpy(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x, cudaMemcpyHostToDevice);
+                if constexpr (_OutputOnHost) memcpy(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x);
+                else cudaMemcpy(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x, cudaMemcpyHostToDevice);
             else
-                if constexpr (_OutputOnHost) cudaMemcpy(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x, cudaMemcpyDeviceToHost);
-                else cudaMemcpy(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x, cudaMemcpyDeviceToDevice);
+                if constexpr (_OutputOnHost) cudaMemcpy(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x, cudaMemcpyDeviceToHost);
+                else cudaMemcpy(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x, cudaMemcpyDeviceToDevice);
             return;
         }
         size_t elementNum = RangeDimensions[_VectorLength - 1];
@@ -88,7 +88,7 @@ __host__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<ui
             size_t optIdx = CoordinatesToIndex<size_t, uint32_t, _VectorLength, true>(OutputDimensions, RangeInOutputsCoordinates + i);
 
             const _T* iptPtr = Input + iptIdx;
-            _T* optPtr = output + optIdx;
+            _T* optPtr = Output + optIdx;
 
             if constexpr (_InputOnHost)
                 if constexpr (_OutputOnHost) memcpy(optPtr, iptPtr, memcpySize);
@@ -112,7 +112,7 @@ __host__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<ui
 }
 #ifdef __CUDACC__
 template <typename _T, size_t _VectorLength, bool _Wrap>
-__device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
+__device__ void bcuda::CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
     using vector_t = FixedVector<uint32_t, _VectorLength>;
 
     if constexpr (_Wrap) {
@@ -132,7 +132,7 @@ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<
                 rangeInOutputCoordinates[j] = landmark.outputIndex;
             }
 
-            CopyBlock<_T, _VectorLength, false>(Input, output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
+            CopyBlock<_T, _VectorLength, false>(Input, Output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
 
             bool toBreak = true;
             for (size_t j = 0; j < _VectorLength; ++j) {
@@ -148,7 +148,7 @@ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<
     }
     else {
         if constexpr (_VectorLength == 1) {
-            memcpy(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x);
+            memcpy(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, sizeof(_T) * RangeDimensions.x);
             return;
         }
         size_t elementNum = RangeDimensions[_VectorLength - 1];
@@ -159,7 +159,7 @@ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<
             size_t optIdx = CoordinatesToIndex<size_t, uint32_t, _VectorLength, true>(OutputDimensions, RangeInOutputsCoordinates + i);
 
             const _T* iptPtr = Input + iptIdx;
-            _T* optPtr = output + optIdx;
+            _T* optPtr = Output + optIdx;
 
             memcpy(optPtr, iptPtr, memcpySize);
 
@@ -179,7 +179,7 @@ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<
 #endif
 
 template <typename _T, size_t _VectorLength, bcuda::copyValFunc_t<_T> _CopyValFunc, bool _Wrap>
-__host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
+__host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
     using vector_t = FixedVector<uint32_t, _VectorLength>;
 
     if constexpr (_Wrap) {
@@ -199,7 +199,7 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
                 rangeInOutputCoordinates[j] = landmark.outputIndex;
             }
 
-            CopyBlock<_T, _VectorLength, _CopyValFunc, false>(Input, output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
+            CopyBlock<_T, _VectorLength, _CopyValFunc, false>(Input, Output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
 
             bool toBreak = true;
             for (size_t j = 0; j < _VectorLength; ++j) {
@@ -215,8 +215,8 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
     }
     else {
         if constexpr (_VectorLength == 1) {
-            for (_T* ipu = Input + RangeDimensions.x; Input < ipu; ++Input, ++output)
-                _CopyValFunc(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x);
+            for (_T* ipu = Input + RangeDimensions.x; Input < ipu; ++Input, ++Output)
+                _CopyValFunc(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x);
             return;
         }
         size_t elementNum = RangeDimensions[_VectorLength - 1];
@@ -226,7 +226,7 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
             size_t optIdx = CoordinatesToIndex<size_t, uint32_t, _VectorLength, true>(OutputDimensions, RangeInOutputsCoordinates + i);
 
             const _T* iptPtr = Input + iptIdx;
-            _T* optPtr = output + optIdx;
+            _T* optPtr = Output + optIdx;
 
             for (_T* ipu = iptPtr + elementNum; iptPtr < ipu; ++iptPtr, ++optPtr)
                 _CopyValFunc(optPtr, iptPtr);
@@ -246,7 +246,7 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
 }
 
 template <typename _T, size_t _VectorLength, bcuda::copyArrFunc_t<_T> _CopyArrFunc, bool _Wrap>
-__host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
+__host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* Output, const FixedVector<uint32_t, _VectorLength>& InputDimensions, const FixedVector<uint32_t, _VectorLength>& OutputDimensions, const FixedVector<uint32_t, _VectorLength>& RangeDimensions, const FixedVector<uint32_t, _VectorLength>& RangeInInputsCoordinates, const FixedVector<uint32_t, _VectorLength>& RangeInOutputsCoordinates) {
     using vector_t = FixedVector<uint32_t, _VectorLength>;
 
     if constexpr (_Wrap) {
@@ -266,7 +266,7 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
                 rangeInOutputCoordinates[j] = landmark.outputIndex;
             }
 
-            CopyBlock<_T, _VectorLength, _CopyArrFunc, false>(Input, output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
+            CopyBlock<_T, _VectorLength, _CopyArrFunc, false>(Input, Output, InputDimensions, OutputDimensions, rangeDimensions, rangeInInputCoordinates, rangeInOutputCoordinates);
 
             bool toBreak = true;
             for (size_t j = 0; j < _VectorLength; ++j) {
@@ -282,7 +282,7 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
     }
     else {
         if constexpr (_VectorLength == 1) {
-            _CopyArrFunc(output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, RangeDimensions.x);
+            _CopyArrFunc(Output + RangeInOutputsCoordinates.x, Input + RangeInInputsCoordinates.x, RangeDimensions.x);
             return;
         }
         size_t elementNum = RangeDimensions[_VectorLength - 1];
@@ -291,7 +291,7 @@ __host__ __device__ void bcuda::CopyBlock(const _T* Input, _T* output, const Fix
             size_t iptIdx = CoordinatesToIndex<size_t, uint32_t, _VectorLength, true>(InputDimensions, RangeInInputsCoordinates + i);
             size_t optIdx = CoordinatesToIndex<size_t, uint32_t, _VectorLength, true>(OutputDimensions, RangeInOutputsCoordinates + i);
 
-            _CopyArrFunc(output + optIdx, Input + iptIdx, elementNum);
+            _CopyArrFunc(Output + optIdx, Input + iptIdx, elementNum);
 
             bool toBreak = true;
             for (size_t j = 1; j < _VectorLength; ++j) {
