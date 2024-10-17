5250f29bedd7cbdd9ca9bb5e03802652a56772d0
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-10-16 21:46:29 -0400

    Enforced conventions for file `points.h`

--- points.h
+++ points.h
@@ -6,42 +6,37 @@
 
 namespace bcuda {
     template <std::unsigned_integral _TIndex, std::unsigned_integral _TVectorElement, size_t _VectorLength, bool _RowMajor>
-    __host__ __device__ constexpr __forceinline _TIndex CoordinatesToIndex(FixedVector<_TVectorElement, _VectorLength> Dimensions, FixedVector<_TVectorElement, _VectorLength> Coordinates);
+    __host__ __device__ constexpr __forceinline _TIndex CoordinatesToIndex(FixedVector<_TVectorElement, _VectorLength> Dimensions, FixedVector<_TVectorElement, _VectorLength> Coordinates) {
+        _TIndex idx = 0;
+        if constexpr (_RowMajor) {
+            for (size_t i = _VectorLength - 1; i != (size_t)-1; --i) {
+                idx *= Dimensions[i];
+                idx += Coordinates[i];
+            }
+        }
+        else {
+            for (size_t i = 0; i < _VectorLength; ++i) {
+                idx *= Dimensions[i];
+                idx += Coordinates[i];
+            }
+        }
+        return idx;
+    }
     template <std::unsigned_integral _TIndex, std::unsigned_integral _TVectorElement, size_t _VectorLength, bool _RowMajor>
-    __host__ __device__ constexpr __forceinline FixedVector<_TVectorElement, _VectorLength> IndexToCoordinates(FixedVector<_TVectorElement, _VectorLength> Dimensions, _TIndex Index);
-}
-
-template <std::unsigned_integral _TIndex, std::unsigned_integral _TVectorElement, size_t _VectorLength, bool _RowMajor>
-__host__ __device__ constexpr __forceinline _TIndex bcuda::CoordinatesToIndex(FixedVector<_TVectorElement, _VectorLength> Dimensions, FixedVector<_TVectorElement, _VectorLength> Coordinates) {
-    _TIndex idx = 0;
-    if constexpr (_RowMajor) {
-        for (size_t i = _VectorLength - 1; i != (size_t)-1; --i) {
-            idx *= Dimensions[i];
-            idx += Coordinates[i];
+    __host__ __device__ constexpr __forceinline FixedVector<_TVectorElement, _VectorLength> IndexToCoordinates(FixedVector<_TVectorElement, _VectorLength> Dimensions, _TIndex Index) {
+        FixedVector<_TVectorElement, _VectorLength> r;
+        if constexpr (_RowMajor) {
+            for (size_t i = _VectorLength - 1; i != (size_t)-1; ++i) {
+                r[i] = Index % Dimensions[i];
+                Index /= Dimensions[i];
+            }
         }
-    }
-    else {
-        for (size_t i = 0; i < _VectorLength; ++i) {
-            idx *= Dimensions[i];
-            idx += Coordinates[i];
+        else {
+            for (size_t i = 0; i < _VectorLength; ++i) {
+                r[i] = Index % Dimensions[i];
+                Index /= Dimensions[i];
+            }
         }
+        return r;
     }
-    return idx;
-}
-template <std::unsigned_integral _TIndex, std::unsigned_integral _TVectorElement, size_t _VectorLength, bool _RowMajor>
-__host__ __device__ constexpr __forceinline bcuda::FixedVector<_TVectorElement, _VectorLength> bcuda::IndexToCoordinates(FixedVector<_TVectorElement, _VectorLength> Dimensions, _TIndex Index) {
-    FixedVector<_TVectorElement, _VectorLength> r;
-    if constexpr (_RowMajor) {
-        for (size_t i = _VectorLength - 1; i != (size_t)-1; ++i) {
-            r[i] = Index % Dimensions[i];
-            Index /= Dimensions[i];
-        }
-    }
-    else {
-        for (size_t i = 0; i < _VectorLength; ++i) {
-            r[i] = Index % Dimensions[i];
-            Index /= Dimensions[i];
-        }
-    }
-    return r;
 }
